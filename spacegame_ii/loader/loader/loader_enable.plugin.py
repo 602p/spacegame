d8e04c097af98489068a515f4fceb3a8c1aa82ac
Louis Goessling <louis@goessling.com>
2015-01-07 12:34:05 -0600

    Reorganize the example mod

--- spacegame_ii/loader/loader/loader_enable.plugin.py
+++ spacegame_ii/loader/loader/loader_enable.plugin.py
@@ -1,2 +1,6 @@
+STATUS = True
+
+from logging import debug, info, warning, error, critical
 def init_enableloader(root):
-	root.settings["graphics"]["loader_screen"]=True
+	info("loader_enable::init_enableloader: Turning the loading screen "+"on" if STATUS else "off")
+	root.settings["graphics"]["loader_screen"]=STATUS
