148b5bea8ab867f947f2866397d4f833785b0f72
The Rambler <littlestllama6@gmail.com>
2015-01-26 11:57:44 -0600

    Easter egg test

---
+++ spacegame_ii/extensions/sebastianmod/supersecreteasteregg.plugin.py
@@ -0,0 +1,19 @@
+import extention_loader, pygame
+import extention_loader, pygame, ui_states, item
+from logging import debug, info, warning, error, critical
+
+SECRET_KEY = pygame.K_F9
+
+class supersecreteasteregg(extention_loader.HookableExtention):
+	def __init__(self, root):
+		self.root=root
+	def event_state(self, state, event):
+		if state=="game":
+			if event.type==pygame.KEYDOWN:
+				if event.key==SECRET_KEY:
+					print("Success!")
+
+def init_inject(root, console):
+	info("Injecting the SecretHook")
+	extention_loader.safepost(console, "[sebastianmod:supersecreteasteregg]: Injecting the SecretHook", color=(0,255,255), bold=1)                                                                                           
+	root.extentions["x_test_secret"]=supersecreteasteregg(root)
